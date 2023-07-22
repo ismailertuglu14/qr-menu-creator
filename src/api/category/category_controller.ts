78bfb2b343144228d2815a6f9fca20bec87f55fe
ismailertuglu <ismailertgl@gmail.com>
2023-07-23 01:31:46 +0300

    removed unused function

--- src/api/category/category_controller.ts
+++ src/api/category/category_controller.ts
@@ -4,7 +4,7 @@ import { ResponseStatus } from "../../core/constants/response_status_enum";
 import NotFoundException from "../../core/exceptions/not_found_exception";
 
 // Entities
-import CategoryModel from "./category_model";
+import CategoryModel from "./models/category_model";
 import RestaurantModel from "../restaurant/restaurant_model";
 import ProductModel from "../product/product_model";
 
@@ -269,40 +269,11 @@ async function deleteCategory(req: Request, res: Response) {
     res.status(500).json(BaseResponse.fail(error.message, error.statusCode));
   }
 }
-async function changePosition(req: Request, res: Response) {
-  try {
-    const { restaurantId, categoryId, newPosition } = req.body;
 
-    // find the greates position parameter category
-    const greatestPositionCategory = await CategoryModel.findOne({
-      restaurantId,
-    }).sort({ position: -1 });
-
-    // find the category to be changed
-    const category = await CategoryModel.findOne({
-      _id: categoryId,
-      restaurantId,
-    });
-
-    await CategoryModel.updateMany(
-      {
-        restaurantId,
-      },
-      {
-        $set: {
-          position: newPosition,
-        },
-      }
-    );
-  } catch (error) {
-    res.send(500).json(BaseResponse.fail(error.message, error.statusCode));
-  }
-}
 export {
   relocateCategory,
   customerGetCategories,
   restaurantGetCategories,
   createCategory,
   deleteCategory,
-  changePosition,
 };

b17c8ab12e3cbc1060b2ffa09392a60e4cd38245
ismailertuglu <ismailertgl@gmail.com>
2023-07-23 01:32:30 +0300

    code refactor

--- src/core/connection/azure_connection.ts
+++ src/core/connection/azure_connection.ts
@@ -1,7 +1,9 @@
 import { BlobServiceClient } from "@azure/storage-blob";
-const x = process.env.AZURE_CONNECTION;
+import dotenv from "dotenv";
+dotenv.config();
+
 const blobServiceClient = BlobServiceClient.fromConnectionString(
-  "DefaultEndpointsProtocol=https;AccountName=qrmenu1923;AccountKey=+N9vbckpO0qEPkKJ8hFy8PAksFaF74cRzvJXXbFtkZiTKFX1ZqUfVPeOmGPJoP3hpbeaU4G1wNhC+ASt/NUp+Q==;EndpointSuffix=core.windows.net"
+  process.env.AZURE_CONNECTION
 );
 
 export default blobServiceClient;
